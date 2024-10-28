6a88186149ce4bae753561d58c78e89256875fdb
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2024-10-28 22:48:07 +0800

    prepare basic export pdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<h1 class="font-bold">Welcome to SvelteKit</h1>
-<section>
-    <button>test</button>
-</section>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-full flex-col items-center justify-center text-center">
+	<p class="font-bold">Math Drill</p>
+	<a role="button" href="./api" target="_blank">test print here</a>
+    <br>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
