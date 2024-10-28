76c02db36a88f46f731f0b8e345e3916d246fcc0
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2024-10-28 12:24:39 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="font-bold">Welcome to SvelteKit</h1>
+<section>
+    <button>test</button>
+</section>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
