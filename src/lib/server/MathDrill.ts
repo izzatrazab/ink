c9ce9a8149e8a69113d3fa6c6292aaffc241fcab
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2024-11-06 21:54:17 +0800

    create MathDrill class

---
+++ src/lib/server/MathDrill.ts
@@ -0,0 +1,88 @@
+import PDFDocument from 'pdfkit';
+
+/**
+ * custom class extending PDFDocument with additional mathematical drawings.
+ */
+export default class MathDrill extends PDFDocument {
+	constructor(constructor:any) {
+		super(constructor);
+	}
+
+	/**
+     * rindu org tu 💕
+	 * @param x coordinate x
+	 * @param y coordinate y
+	 * @param num1 first number in the equation
+	 * @param num2 second number in the equation
+	 * @param operation the operation symbol (+, -, x)
+	 * @param width width of the column method (box ??, ibarat mcm kotak).
+	 */
+	drawColumnMethod(
+		x: number,
+		y: number,
+		num1: number,
+		num2: number,
+		operation: string,
+		width: number,
+		questionNumber: number,
+		answer: boolean,
+		padding: number = 5
+	) {
+		const content_width = width - padding - padding;
+		const content_x = x + padding;
+		const content_y = y + padding;
+
+		// Draw question number
+		this.text(questionNumber.toString() + ')', content_x, content_y, {
+			width: width,
+			align: 'left'
+		});
+
+		// Draw first number
+		this.text(num1.toString(), content_x, content_y + 15, {
+			width: content_width,
+			align: 'right'
+		});
+
+		// Draw operation sign
+		this.text(operation, content_x + 20, content_y + 30, {
+			width: content_width,
+			align: 'left'
+		});
+
+		// Draw second number
+		this.text(num2.toString(), content_x, content_y + 30, {
+			width: content_width,
+			align: 'right'
+		});
+
+		// Draw line
+		const lineY = content_y + 50;
+
+		// this.rect(x, y, width, 70) // temporary
+
+		this
+			.moveTo(content_x + 20, lineY)
+			.lineTo(content_x + content_width, lineY)
+			.stroke();
+
+		this
+			.moveTo(content_x + 20, lineY + 20)
+			.lineTo(content_x + content_width, lineY + 20)
+			.stroke();
+
+		// Calculate and write the answer
+		if (answer) {
+			// Calculate and write the answer
+			const result = operation === '+' ? num1 + num2 : num1 - num2;
+			this.text(result.toString(), content_x, content_y + 55, {
+				width: content_width,
+				align: 'right'
+			});
+		}
+
+		// const result = operation === '+' ? num1 + num2 : num1 - num2;
+		// this.text(result.toString(), x+30, lineY + 10);
+		// console.dir(lineY);
+	}
+}
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,13 +1,14 @@
-import PDFDocument from 'pdfkit';
+import MathDrill from '$lib/server/MathDrill';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
 	// const min = Number(url.searchParams.get('min') ?? '0');
 	// const max = Number(url.searchParams.get('max') ?? '1');
-	const doc = new PDFDocument({ size: 'A4' });
+	const doc = new MathDrill({ size: 'A4' });
 	/**
 	 * size: 'A4',
-	 * width: 595.28, height: 841.89, (izzat x tahu metric dia pakai apa, dan value ni dah include margin) - mungkin guna PostScript point (tp x tahu betul ke x)
+	 * width: 595.28, height: 841.89, (izzat x tahu metric dia pakai apa, dan value ni dah include margin)
+	 * - mungkin guna PostScript point (tp x tahu betul ke x)
 	 * default margin: margins: { top: 72, left: 72, bottom: 72, right: 72 },
 	 */
 
@@ -22,85 +23,92 @@ export const GET: RequestHandler = async ({ url }) => {
 	// here is where we need to RnD
 
 	const origin_x = 72;
-	const origin_y = 72
-	const page_content_width = doc.page.width - doc.page.margins.left - doc.page.margins.right
-	const page_content_height = doc.page.height - doc.page.margins.top - doc.page.margins.bottom
-	let tempX:number = origin_x;
-	let tempY:number = origin_y;
-
+	const origin_y = 72;
+	const page_content_width = doc.page.width - doc.page.margins.left - doc.page.margins.right;
+	const page_content_height = doc.page.height - doc.page.margins.top - doc.page.margins.bottom;
+	let tempX: number = origin_x;
+	let tempY: number = origin_y;
 
 	const column = 5; // for now 5
 	/**
 	 * @var column_width - width of each column
 	 */
-	const column_width = page_content_width/column; // each column width
+	const column_width = page_content_width / column; // each column width
 
 	const row = 5; // for now 5
 	/**
 	 * @var row_height width of each column
 	 */
-	const row_height = page_content_height/row; // each column width
+	const row_height = page_content_height / row; // each column width
 
-	let tempCMW:number = column_width - 10; // temporary column method width
+	let tempCMW: number = column_width - 10; // temporary column method width
 	let counter = 0;
 
 	// calculate x shift
-	let x_shift:number = ( column_width - tempCMW ) / 2;
+	let x_shift: number = (column_width - tempCMW) / 2;
 
 	// To assign the random numbers generated into the arrays
-    let array1: number[] = [];
+	let array1: number[] = [];
 	let array2: number[] = [];
 
 	let answerSheet: boolean = false;
 
 	for (let index = 0; index < 5; index++) {
-		for(let j=0; j<5; j++) {
-
+		for (let j = 0; j < 5; j++) {
 			/** start generating random questions */
 			let firstNumDigit = 3; // for now 3 (boleh tukar samada 1/2/3)
 			let secondNumDigit = 3; // for now 3 (boleh tukar samada 1/2/3)
 
 			var firstNum = 0;
 			var secondNum = 0;
-			
-				// options to choose the number of digit for the first number
-				switch(firstNumDigit) {
-					case 1:  
-						firstNum = randomQuestionsMethod(1, 9);
-						break;
-					case 2: 
-						firstNum = randomQuestionsMethod(10, 99);
-						break;
-					case 3:
-						firstNum = randomQuestionsMethod(100, 999);
-						break;
-					default:
-						break;
-				}
-						
-				// options to choose the number of digit for the second number
-				switch(secondNumDigit) {
-					case 1:  
-						secondNum = randomQuestionsMethod(1, 9);
-						break;
-					case 2: 
-						secondNum = randomQuestionsMethod(10, 99);
-						break;
-					case 3:
-						secondNum = randomQuestionsMethod(100, 999);
-						break;
-					default:
-						break;
-				}
 
-				array1.push(firstNum);
-				array2.push(secondNum);
+			// options to choose the number of digit for the first number
+			switch (firstNumDigit) {
+				case 1:
+					firstNum = randomQuestionsMethod(1, 9);
+					break;
+				case 2:
+					firstNum = randomQuestionsMethod(10, 99);
+					break;
+				case 3:
+					firstNum = randomQuestionsMethod(100, 999);
+					break;
+				default:
+					break;
+			}
+
+			// options to choose the number of digit for the second number
+			switch (secondNumDigit) {
+				case 1:
+					secondNum = randomQuestionsMethod(1, 9);
+					break;
+				case 2:
+					secondNum = randomQuestionsMethod(10, 99);
+					break;
+				case 3:
+					secondNum = randomQuestionsMethod(100, 999);
+					break;
+				default:
+					break;
+			}
+
+			array1.push(firstNum);
+			array2.push(secondNum);
 
 			/** end of generating random questions */
 
 			// var firstNum = randomQuestionsMethod(100, 999);
 			// var secondNum = randomQuestionsMethod(100, 999);
-			drawColumnMethod(doc, origin_x + x_shift + j*column_width, origin_y + index*row_height, array1[counter], array2[counter], '+', tempCMW, ++counter, answerSheet);
+			doc.drawColumnMethod(
+				origin_x + x_shift + j * column_width,
+				origin_y + index * row_height,
+				array1[counter],
+				array2[counter],
+				'+',
+				tempCMW,
+				++counter,
+				answerSheet
+			);
 		}
 	}
 
@@ -111,17 +119,26 @@ export const GET: RequestHandler = async ({ url }) => {
 			align: 'left'
 		})
 		.underline(76, 90, 76, 5);
-		// .registerFont('GoodDog','fonts/GoodDog.ttf')
-		// .font('fonts/GoodDog.ttf');
+	// .registerFont('GoodDog','fonts/GoodDog.ttf')
+	// .font('fonts/GoodDog.ttf');
 
-		counter = 0; // reset counter to zero
-		answerSheet = true;
+	counter = 0; // reset counter to zero
+	answerSheet = true;
 
-		for (let index = 0; index < 5; index++) {
-			for (let j = 0; j < 5; j++) {
-				drawColumnMethod(doc, origin_x + x_shift + j*column_width, (origin_y + index*row_height) + 30, array1[counter], array2[counter], '+', tempCMW, ++counter, answerSheet);
-			}
+	for (let index = 0; index < 5; index++) {
+		for (let j = 0; j < 5; j++) {
+			doc.drawColumnMethod(
+				origin_x + x_shift + j * column_width,
+				origin_y + index * row_height + 30,
+				array1[counter],
+				array2[counter],
+				'+',
+				tempCMW,
+				++counter,
+				answerSheet
+			);
 		}
+	}
 
 	/** end add content to the PDF */
 
@@ -145,95 +162,10 @@ export const GET: RequestHandler = async ({ url }) => {
 };
 
 /**
- * 
- * @param doc PDFDocument
- * @param x coordinate x
- * @param y coordinate y
- * @param num1 first number in the equation
- * @param num2 second number in the equation
- * @param operation the operation symbol (+, -, x)
- * @param width width of the column method (box ??, ibarat mcm kotak).
+ * @param min first number in the equation
+ * @param max second number in the equation
  */
 
-function drawColumnMethod(
-	doc: typeof PDFDocument,
-	x: number,
-	y: number,
-	num1: number,
-	num2: number,
-	operation: string,
-	width: number,
-	questionNumber: number,
-	answer: boolean,
-	padding: number = 5,
-) {
-	const content_width = width - padding - padding;
-	const content_x = x + padding;
-	const content_y = y + padding;
-
-	// Draw question number
-	doc.text(questionNumber.toString() + ')' , content_x, content_y, {
-		width: width,
-		align: 'left'
-	});
-
-	// Draw first number
-	doc.text(num1.toString(), content_x, content_y + 15, {
-		width: content_width,
-		align: 'right'
-	});
-	 
-	// Draw operation sign
-	doc.text(operation, content_x + 20, content_y + 30, {
-		width: content_width,
-		align: 'left'
-	});
-
-	// Draw second number
-	doc.text(num2.toString(), content_x, content_y + 30, {
-		width: content_width,
-		align: 'right'
-	});
-
-	// Draw line
-	const lineY = content_y + 50;
-
-	// doc.rect(x, y, width, 70) // temporary
-
-	doc
-		.moveTo(content_x + 20, lineY)
-		.lineTo(content_x + content_width, lineY)
-		.stroke();
-
-	doc
-		.moveTo(content_x + 20, lineY + 20)
-		.lineTo(content_x + content_width, lineY + 20)
-		.stroke();
-
-	// Calculate and write the answer
-	if (answer) {
-		// Calculate and write the answer
-		const result = operation === '+' ? num1 + num2 : num1 - num2;
-		doc.text(result.toString(), content_x, content_y + 55, {
-			width: content_width,
-			align: 'right'
-		});
-		// console.dir(lineY);
-	}
-	// const result = operation === '+' ? num1 + num2 : num1 - num2;
-	// doc.text(result.toString(), x+30, lineY + 10);
-	// console.dir(lineY);
-	
-}
-
-/**
-* @param min first number in the equation
-* @param max second number in the equation
-*/
-
-function randomQuestionsMethod(
-	min: number,
-	max: number,
-) : number {
-	return Math.floor(Math.random() * (max - min + 1) + min)
+function randomQuestionsMethod(min: number, max: number): number {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
