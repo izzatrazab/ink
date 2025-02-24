67bf3fcba4f4022228caea0a3c5bdbd9dedf08b8
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2025-02-24 23:41:46 +0800

    expose long division

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,80 +1,84 @@
 <script>
-	import { dev } from '$app/environment';
+	// import { dev } from '$app/environment';
 </script>
 
 <svelte:head>
 	<title>Column Method Drills</title>
 	<meta name="description" content="Generate column method drills." />
 </svelte:head>
-<div class="container">
-	{#if dev}
-		<div>
-			<hgroup>
-				<h1>Long Division Method</h1>
-				<p>test</p>
-			</hgroup>
-			<article>
-				<header>Generate Long Division Method Drills</header>
+<div class="container" style="padding: 2rem;">
+	<div>
+		<hgroup>
+			<h1>Long Division Method</h1>
+			<p>
+				Long division is a way to divide big numbers by breaking it down into smaller steps. You see
+				how many times the divisor (the number you're dividing by) fits into parts of the dividend
+				(the number you're dividing), write the result, subtract, and then bring down the next
+				number. You keep going until you’ve finished. The result is the answer, and any leftover is
+				the remainder.
+			</p>
+		</hgroup>
+		<article>
+			<header>Generate Long Division Method Drills</header>
 
-				<form
-					action="./long-division-method"
-					method="GET"
-					id="generate-long-division-drill"
-					target="_blank"
-				>
-					<fieldset class="grid">
-						<label for="difficulty"
-							>Difficulty
-							<select
-								name="difficulty"
-								placeholder="Select difficulty"
-								aria-label="Select difficulty"
-							>
-								<option selected value="easy">Easy</option>
-								<option value="medium">Medium</option>
-								<option value="hard">Hard</option>
-							</select>
-						</label>
-						<fieldset>
-							<legend>Remainder</legend>
-							<input type="radio" id="has-remainder" name="remainder" value='true' checked />
-							<label for="has-remainder">Has Remainder</label>
-							<input type="radio" id="no-remainder" name="remainder" value='false' />
-							<label for="no-remainder">No Remainder</label>
-						</fieldset>
-					</fieldset>
-
+			<form
+				action="./long-division-method"
+				method="GET"
+				id="generate-long-division-drill"
+				target="_blank"
+			>
+				<fieldset class="grid">
+					<label for="difficulty"
+						>Difficulty
+						<select
+							name="difficulty"
+							placeholder="Select difficulty"
+							aria-label="Select difficulty"
+						>
+							<option selected value="easy">Easy</option>
+							<option value="medium">Medium</option>
+							<option value="hard">Hard</option>
+						</select>
+					</label>
 					<fieldset>
-						<label for="nop">
-							Number of Pages
-							<input
-								type="number"
-								name="nop"
-								required
-								placeholder="Provide number of pages"
-								aria-label="Number of pages"
-								min="1"
-								list="fibonacci"
-								value="1"
-							/>
-						</label>
-						<datalist id="fibonacci">
-							<option value="1"></option>
-							<option value="2"></option>
-							<option value="3"></option>
-							<option value="5"></option>
-							<option value="8"></option>
-						</datalist>
+						<legend>Remainder</legend>
+						<input type="radio" id="has-remainder" name="remainder" value="true" checked />
+						<label for="has-remainder">Has Remainder</label>
+						<input type="radio" id="no-remainder" name="remainder" value="false" />
+						<label for="no-remainder">No Remainder</label>
 					</fieldset>
-				</form>
-				<footer>
-					<button type="submit" form="generate-long-division-drill" aria-label="Generate"
-						>Generate Drill</button
-					>
-				</footer>
-			</article>
-		</div>
-	{/if}
+				</fieldset>
+
+				<fieldset>
+					<label for="nop">
+						Number of Pages
+						<input
+							type="number"
+							name="nop"
+							required
+							placeholder="Provide number of pages"
+							aria-label="Number of pages"
+							min="1"
+							list="fibonacci"
+							value="1"
+						/>
+					</label>
+					<datalist id="fibonacci">
+						<option value="1"></option>
+						<option value="2"></option>
+						<option value="3"></option>
+						<option value="5"></option>
+						<option value="8"></option>
+					</datalist>
+				</fieldset>
+			</form>
+			<footer>
+				<button type="submit" form="generate-long-division-drill" aria-label="Generate">
+					Generate Drill
+				</button>
+			</footer>
+		</article>
+	</div>
 
 	<div>
 		<hgroup>
