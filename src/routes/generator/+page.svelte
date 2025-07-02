f31be7411831fe0e85e5d0af10e44c725cca4569
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2025-07-03 00:39:13 +0800

    Pengenalan: update content

--- src/routes/generator/+page.svelte
+++ src/routes/generator/+page.svelte
@@ -1,7 +1,37 @@
-<script>
-	import UnderConstruction from "$lib/components/alert/UnderConstruction.svelte";
 
-	// import { dev } from '$app/environment';
-</script>
+<div class="space-y-16">
+	<section>
+		<hgroup>
+			<h1 class="text-4xl font-bold">Pengenalan</h1>
+			<p class="mt-8">
+				Kami menyediakan beberapa jenis latih tubi matematik dalam bentuk PDF. Buat masa ini, laman
+				ini masih dalam fasa pembinaan, dan pilihan latih tubi yang tersedia masih terhad.
+				InshaAllah, jika ada kelapangan masa, lebih banyak jenis latih tubi akan ditambah dari
+				semasa ke semasa.
+			</p>
+		</hgroup>
+	</section>
 
-<UnderConstruction />
+	<section>
+		<hgroup>
+			<h1 class="text-4xl font-bold">Kenapa Pilih Kami ?</h1>
+			<p class="mt-8">
+				Setiap soalan dalam latih tubi kami dijana secara on demand, di mana setiap kali anda memuat
+				turun latih tubi tersebut, soalan yang baru akan dijana. Dengan cara ini, anda boleh
+				mendapatkan soalan yang baru setiap masa.
+			</p>
+		</hgroup>
+		<div class="md:w-3/4 mt-4">
+			<p>Kebaikannya:</p>
+			<ul class="ml-8 list-disc mt-4">
+				<li>
+					Tidak lagi bosan dengan set soalan yang sama - setiap set dijana secara
+					rawak, jadi anda akan mendapat soalan yang unik setiap kali.
+				</li>
+				<li>
+					Menjimatkan masa - tidak perlu cipta soalan sendiri atau cari bahan baharu (terutamanya bagi guru-guru dan ibu bapa yang ingin menyediakan latihan kepada anak-anak / murid mereka).
+				</li>
+			</ul>
+		</div>
+	</section>
+</div>
