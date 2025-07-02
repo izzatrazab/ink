6d657d37742c3eac63720b7260a5dac5255e5b8a
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2025-07-03 07:47:59 +0800

    Pengenalan: update content

--- src/routes/generator/+page.svelte
+++ src/routes/generator/+page.svelte
@@ -2,10 +2,10 @@
 <div class="space-y-16">
 	<section>
 		<hgroup>
-			<h1 class="text-4xl font-bold">Pengenalan</h1>
+			<h1 class="text-4xl font-bold">Pengenalan 👋🏼</h1>
 			<p class="mt-8">
 				Kami menyediakan beberapa jenis latih tubi matematik dalam bentuk PDF. Buat masa ini, laman
-				ini masih dalam fasa pembinaan, dan pilihan latih tubi yang tersedia masih terhad.
+				ini masih dalam <b>fasa pembinaan</b>, dan pilihan latih tubi yang tersedia masih terhad.
 				InshaAllah, jika ada kelapangan masa, lebih banyak jenis latih tubi akan ditambah dari
 				semasa ke semasa.
 			</p>
@@ -14,7 +14,7 @@
 
 	<section>
 		<hgroup>
-			<h1 class="text-4xl font-bold">Kenapa Pilih Kami ?</h1>
+			<h1 class="text-4xl font-bold">Kenapa Pilih Kami&nbsp;? 🤔</h1>
 			<p class="mt-8">
 				Setiap soalan dalam latih tubi kami dijana secara on demand, di mana setiap kali anda memuat
 				turun latih tubi tersebut, soalan yang baru akan dijana. Dengan cara ini, anda boleh
@@ -22,16 +22,28 @@
 			</p>
 		</hgroup>
 		<div class="md:w-3/4 mt-4">
-			<p>Kebaikannya:</p>
+			<h2 class="text-2xl font-semibold">Kebaikannya 🤩:</h2>
 			<ul class="ml-8 list-disc mt-4">
 				<li>
-					Tidak lagi bosan dengan set soalan yang sama - setiap set dijana secara
-					rawak, jadi anda akan mendapat soalan yang unik setiap kali.
+					Tidak lagi bosan dengan set soalan yang sama.
+					<br>
+					Setiap set dijana secara rawak, anda akan mendapat soalan yang unik setiap kali.
 				</li>
 				<li>
-					Menjimatkan masa - tidak perlu cipta soalan sendiri atau cari bahan baharu (terutamanya bagi guru-guru dan ibu bapa yang ingin menyediakan latihan kepada anak-anak / murid mereka).
+					Menjimatkan masa
+					<br>
+					Tidak perlu cipta soalan sendiri atau cari bahan baharu (terutamanya bagi guru-guru dan ibu bapa yang ingin menyediakan latihan kepada anak-anak / murid mereka).
 				</li>
 			</ul>
 		</div>
+	</section>
+		<section>
+		<hgroup>
+			<h1 class="text-4xl font-bold">Hubungi Kami 📨</h1>
+			<p class="mt-8">
+				Jika ada pertanyaan atau cadangan(sangat dialu-alukan). Boleh hantar e-mel kepada kami di  
+				<a href="mailto:inkcraft.space.@gmail.com" class="link-info">inkcraft.space.@gmail.com</a>.
+			</p>
+		</hgroup>
 	</section>
 </div>
